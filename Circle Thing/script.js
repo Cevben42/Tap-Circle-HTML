5f8b91a675206445b601c7f8fd665b8325b59ee2
Carl <143086707+Cevben42@users.noreply.github.com>
2025-07-10 22:29:01 +0800

    Update script.js
    Added comments

--- Circle Thing/script.js
+++ Circle Thing/script.js
@@ -5,17 +5,17 @@ const sizeSlider=document.getElementById("sizeSlider");
 const circle=document.getElementById("circle");
 const counter=document.getElementById("counter");
 const body=document.body;
-
+//Toggle for dark/light mode
 toggleBtn.addEventListener("click", ()=>{
     body.classList.toggle("dark");
 });
-
+//resize circle slider
 sizeSlider.addEventListener("input", (e)=>{
     const size=e.target.value+"px";
     circle.style.width=size;
     circle.style.height=size;
 });
-
+//Click counter
 circle.addEventListener("click", ()=>{
     count++;
     counter.textContent=count;
